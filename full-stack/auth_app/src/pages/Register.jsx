a1a4b779bf4556967ac642d820468024480191ca
RaghvendraPRAJAPATI72 <raghvendraprjapati72@gmail.com>
2026-05-13 01:31:43 +0530

    fixed register api url

--- full-stack/auth_app/src/pages/Register.jsx
+++ full-stack/auth_app/src/pages/Register.jsx
@@ -45,7 +45,7 @@ export default function Register() {
       setLoading(true);
 
       await axios.post(
-        "${import.meta.env.VITE_API_URL}/auth/register",
+        `${import.meta.env.VITE_API_URL}/auth/register`,
         {
           name: data.name,
           email: data.email,
